7af5eec85b4b5585674a32e7d95877912dc9cb3c
tfkhdyt <tfkhdyt@proton.me>
2023-05-03 08:34:31 +0700

    feat(8kyu): add even or odd

---
+++ 8kyu/even_or_odd/src/lib.rs
@@ -0,0 +1,20 @@
+fn even_or_odd(i: i32) -> &'static str {
+    match i % 2 {
+        0 => "Even",
+        _ => "Odd",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(even_or_odd(0), "Even");
+        assert_eq!(even_or_odd(2), "Even");
+        assert_eq!(even_or_odd(1), "Odd");
+        assert_eq!(even_or_odd(7), "Odd");
+        assert_eq!(even_or_odd(-1), "Odd");
+    }
+}
